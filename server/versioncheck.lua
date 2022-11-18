4e2626213e74bfbd69d9a64e3f05a6c7e51b29b4
Moritz Walter <moritz.walter@the-systems.eu>
2022-11-18 19:22:39 +0100

    fix versioncheck

--- server/versioncheck.lua
+++ server/versioncheck.lua
@@ -11,15 +11,12 @@ Citizen.CreateThread( function()
         end
         curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
 
-        if not json['response']['latest_preVersion'] == false then
-            if json['response']['latest_preVersion']['version'] == curVersion then
-                print("You are using the latest preRelease version. Version: ".. curVersion)
-            elseif json['response']['latest_version']['version'] == curVersion then
-                print("You are using the latest version. Version: ".. curVersion)
-            else
-                print("You are using an outdated version. Current-Version: ".. curVersion .. " | Latest-Version: " .. json['response']['latest_version']['version'])
-                print("Download it here: https://github.com/MaticRP/scenemenu")
-            end
+
+       if json['response']['latest_version']['version'] == curVersion then
+            print("You are using the latest version. Version: ".. curVersion)
+        else
+            print("You are using an outdated version. Current-Version: ".. curVersion .. " | Latest-Version: " .. json['response']['latest_version']['version'])
+            print("Download it here: https://github.com/MaticRP/scenemenu")
         end
     end
     PerformHttpRequest("https://01-services.the-systems.eu/api/v1/service/6", checkVersion, "GET")
